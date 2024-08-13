c22848857e282b223119a934f76e72eaac81d8f9
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-08-13 14:28:40 +0530

    Time: 0 ms (100%), Space: 43.7 MB (92.94%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -6,16 +6,37 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+ solution for the problem:-
+ListNode prev = head;
+        ListNode temp = prev.next;
+
+        while(temp != null){
+            if(temp.val == prev.val){
+                temp = temp.next;
+                continue;
+            }
+
+            prev.next = temp;
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = null; 
+        return head;
  * }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode list = head;         
-        while(list != null) {
-            if (list.next == null) break; 
-            if (list.val == list.next.val) list.next = list.next.next;
-            else list = list.next;
-        }         
+            ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
         return head;
     }
 }
