322ee69af3a6c6abef0ba91d3fb8c8ad96570370
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-08-18 20:49:02 +0530

    Time: 67 ms (48.44%), Space: 63 MB (25.15%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-               int[] result = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>(); // stores pair of (temperature, index)
+             Stack<Integer> helperStack = new Stack<>();
+             int n = temperatures.length;
+             int[] result = new int[n];
 
-        for (int i = 0; i < temperatures.length; i++) {
-            int temp = temperatures[i];
-            while (!stack.isEmpty() && temp > stack.peek()[0]) {
-                int[] pop = stack.pop();
-                result[pop[1]] = i - pop[1];
+             for (int idx = 0; idx < n; idx++) {
+            // Ensure stack operations are valid
+            while (!helperStack.isEmpty() && temperatures[idx] > temperatures[helperStack.peek()]) {
+                int prevIdx = helperStack.pop();
+                result[prevIdx] = idx - prevIdx;
             }
-            stack.push(new int[]{temp, i});
+            helperStack.push(idx);
+        }
+
+        // Remaining indices in the stack have no greater temperatures
+        while (!helperStack.isEmpty()) {
+            result[helperStack.pop()] = 0;
         }
 
         return result;
