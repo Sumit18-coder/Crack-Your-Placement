c66ede63de89ec5968be339700aad2f8f5929567
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-08-27 22:25:42 +0530

    Time: 16 ms (60.7%), Space: 44 MB (90.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-        int ans = dpfunc(0,coins,amount,dp,n);
-        if(ans == Integer.MAX_VALUE-1) return -1;
-        return ans;
-    }
-        public int dpfunc(int ind, int[] coins, int amount, int[][] dp, int n){
-        if(ind== n && amount == 0) return 0;
-        if(amount < 0) return Integer.MAX_VALUE-1;
-        if(ind == n) return Integer.MAX_VALUE-1;
-
-        if(dp[ind][amount] != -1) return dp[ind][amount]; 
-
-        dp[ind][amount] = Math.min(1+dpfunc(ind,coins,amount-coins[ind],dp,n),dpfunc(ind+1,coins,amount,dp,n));
-        return dp[ind][amount];
+     if(amount < 1) return 0;
+     //create a dp
+     int[] minCoinsDp = new int[amount + 1];
+     for(int i = 1;i <= amount; i++){
+        minCoinsDp[i] = Integer.MAX_VALUE;
+        for(int coin : coins)
+            if(coin <= i && minCoinsDp[i - coin] != Integer.MAX_VALUE)
+               minCoinsDp[i] = Math.min(minCoinsDp[i],1 + minCoinsDp[i - coin]); 
+     } 
+     if(minCoinsDp[amount] == Integer.MAX_VALUE)
+        return -1;
+     return minCoinsDp[amount];
     }
 }
