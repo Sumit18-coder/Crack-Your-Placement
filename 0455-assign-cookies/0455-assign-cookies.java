edeb2d50fd36263d24d28ba431a046f19e78c524
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-10-22 19:07:29 +0530

    Time: 10 ms (78.97%), Space: 48.3 MB (58.98%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int j = 0;
+        int ans = 0;
+        for(int i : g){
+            while(j < s.length && s[j] < i){
+                j++;
+            }
+            if(j == s.length) return ans;
+            else{
+                j++;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
