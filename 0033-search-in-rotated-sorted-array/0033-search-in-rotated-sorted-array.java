c70868d2426337346f5136d0dc7e64eaa66a0276
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-10-24 00:24:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (20.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int search(int[] arr,int target){
-        if(arr == null || arr.length == 0) return -1;
-        return modifiedSearch(arr,target,0,arr.length-1);
-    }
-    private int modifiedSearch(int[] arr, int target,int left,int right) {
-        //not found
-        if(left > right)
-            return -1;
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
 
-        int mid = left + ((right - left)/2);
-        if(arr[mid] == target)
-            return mid;
-        
-        if(arr[mid] >= arr[left]){
-           if(arr[left] <= target && target <= arr[mid])
-            return modifiedSearch(arr,target,left,mid - 1);
-           else
-            return modifiedSearch(arr,target,mid+1,right);
-        }else{
-            if(arr[mid] <= target && target <= arr[right])
-              return modifiedSearch(arr,target,mid+1,right);
-            else
-              return modifiedSearch(arr,target,left,mid-1);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else { // Right half is sorted
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
         }
+        return -1;
     }
 }
