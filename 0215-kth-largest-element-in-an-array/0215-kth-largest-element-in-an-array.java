65c7d89e0813447593fe9fbc59d0ec7553adc670
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-11-12 19:34:21 +0530

    Time: 35 ms (76.82%), Space: 62.1 MB (5.22%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,37 +1,25 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        Random rand = new Random();
-        while (true) {
-            int pivot_index = left + rand.nextInt(right - left + 1);
-            int new_pivot_index = partition(nums, left, right, pivot_index);
-            if (new_pivot_index == nums.length - k) {
-                return nums[new_pivot_index];
-            } else if (new_pivot_index > nums.length - k) {
-                right = new_pivot_index - 1;
-            } else {
-                left = new_pivot_index + 1;
+        int main_max = Integer.MIN_VALUE;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
             }
+            main_max = Math.max(main_max,nums[i]);
         }
-    }
-
-    private int partition(int[] nums, int left, int right, int pivot_index) {
-        int pivot = nums[pivot_index];
-        swap(nums, pivot_index, right);
-        int stored_index = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, i, stored_index);
-                stored_index++;
-            }
+        if(map.size() == 1){
+            return main_max;
         }
-        swap(nums, right, stored_index);
-        return stored_index;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        k -= map.get(main_max);
+        while(k > 0){
+             main_max--;
+             if(map.containsKey(main_max)){
+                k -= map.get(main_max);
+             }
+        }
+        return main_max;
     }
 }
