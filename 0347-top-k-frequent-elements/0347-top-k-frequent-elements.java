8ed8339ccbf734dd60ca90dde7baf290d54f9840
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2024-11-12 22:22:47 +0530

    Time: 13 ms (75.98%), Space: 48.3 MB (66.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
-        Map<Integer,Integer> FrequencyMap = new HashMap<>();
+         List<Integer>[] bucket = new List[nums.length + 1];
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
 
-        for(int n : nums){
-            FrequencyMap.put(n,FrequencyMap.getOrDefault(n,0) + 1);
-        }
-        for(int Key : FrequencyMap.keySet()){
-            int frequency = FrequencyMap.get(Key);
-            if(bucket[frequency] == null){
-                bucket[frequency] = new ArrayList<>();
-            }     
-            bucket[frequency].add(Key);
-        }
-        int[] res = new int[k];
-        int counter = 0;
-        for(int pos = bucket.length - 1; pos >= 0; pos--){
-            if(bucket[pos] != null){
-                for(Integer integer : bucket[pos]){
-                    res[counter++] = integer;
-                    if(counter == k){
-                        return res;
-                    }
-                }
-            }
-        }
-        return res;
+    for (int n : nums) {
+      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+    }
+
+    for (int key : frequencyMap.keySet()) {
+      int frequency = frequencyMap.get(key);
+      if (bucket[frequency] == null) {
+        bucket[frequency] = new ArrayList<>();
+      }
+      bucket[frequency].add(key);
+    }
+
+    List<Integer> topK = new ArrayList<>();
+    for (int pos = bucket.length - 1;pos >= 0 && topK.size() < k; pos--) {
+      if (bucket[pos] != null) {
+        topK.addAll(bucket[pos]);
+      }
+    }
+
+    return topK.stream().mapToInt(i -> i).toArray();
     }
 }
