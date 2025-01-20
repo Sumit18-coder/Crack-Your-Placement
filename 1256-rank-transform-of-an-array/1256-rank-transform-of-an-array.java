255104377d75ec21ea4ea4c944199e673f00aea9
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2025-01-20 19:23:15 +0530

    Time: 7 ms (99.44%), Space: 59.4 MB (93.71%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,28 +1,56 @@
-
 class Solution {
-    public int[] arrayRankTransform(int[] a) {
-        // Use a TreeMap to automatically sort the values
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+    public int[] arrayRankTransform(int[] arr) {
+         if(arr.length == 0) {
+            return new int[]{};
+        }
+        
+        int min = arr[0];
+        int max = arr[0];
+        for(int num : arr) {
+            if(min > num) {
+                min = num;
+            } else if(max < num) {
+                max = num;
+            }
+        }
+        
+        if(max - min > 1000000) { return getRanks(arr); }
 
-        // Store values in the TreeMap
-        for (int val : a) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
+        int[] ranks = new int[max - min + 1];
+        for(int num : arr) {
+            ranks[num - min] = 1;
+        }
+        for(int id = 1; id < ranks.length; id++) {
+            ranks[id] += ranks[id - 1];
+        }
+        int []result = new int[arr.length];
+        int id = 0;
+        for(int num : arr) {
+            result[id++] = ranks[num - min];
+        }
+        return result;
+    }
+
+    private int[] getRanks(int[] arr) {
+        Integer[] indexes = new Integer[arr.length];
+        int[] ranks = new int[arr.length];
+
+        for(int index = 0; index < arr.length; index++) {
+            indexes[index] = index;
         }
 
-        // Assign ranks to the values
-        int rank = 1;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            map.put(entry.getKey(), rank);
-            rank++;
+        Arrays.sort(indexes, (i1, i2) -> arr[i1] - arr[i2]);
+
+        int rank = 0;
+        int prev = arr[0] ^ 1;
+        for(int index = 0; index < indexes.length; index++) {
+            if(prev != arr[indexes[index]]) {
+                ++rank;
+            }
+            ranks[indexes[index]] = rank;
+            prev = arr[indexes[index]];
         }
 
-        // Traverse the array and assign ranks based on the map
-        int[] ans = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            ans[i] = map.get(a[i]);
-        }
-
-        // Return the ranks
-        return ans;
+        return ranks;
     }
 }
