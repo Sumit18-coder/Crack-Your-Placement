d5f7d481232d843d4e3f03e2b55b141ef6733aef
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2025-02-08 18:23:39 +0530

    Time: 8 ms (55.18%), Space: 44.4 MB (80.16%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,28 +1,21 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        HashMap<Integer,Integer> freq = new HashMap<>();
         Integer[] newNums = new Integer[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        for(int i = 0;i < nums.length;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
             newNums[i] = nums[i];
         }
-
-        Arrays.sort(newNums, (n1, n2) -> {
-            if (freq.get(n1) != freq.get(n2)) {
-                // if freq of numbers are not equal then return in increasing order based on
-                // freq.
+        Arrays.sort(newNums,(n1,n2)-> {
+            if(freq.get(n1) != freq.get(n2)){
                 return freq.get(n1) - freq.get(n2);
-            } else {
-                // otherwise sort them in decreasing order based on number in nums.
-                return n2 - n1;
+            }else{
+                return n2-n1;
             }
         });
-
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0;i < nums.length;i++){
             nums[i] = newNums[i];
         }
-
         return nums;
     }
 }
