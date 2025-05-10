8ca19d192734edac6fbd17dab844916782607c5f
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2025-05-10 21:53:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (25.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,8 +1,9 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int i = 0;i < (n + 1)/2 ;i++){
+        for(int i = 0;i < (n + 1)/2;i++){
             for(int j = 0;j < n/2;j++){
+                //temp = bottom left
                 int temp = matrix[n - 1 - j][i];
                 //bottom left = bottom right
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
@@ -13,6 +14,6 @@ class Solution {
                 //top left = temp
                 matrix[i][j] = temp;
             }
-        }  
+        }
     }
 }
