5fa369ec6e2965015cf1d6a007ddb29ee8d4e73b
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2025-08-10 20:28:02 +0530

    Time: 0 ms (100%), Space: 43.3 MB (50.43%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -10,10 +10,10 @@ class Solution {
                 continue;
             }
             if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                if(nums[low] <= target && target <= nums[mid]) high = mid  - 1;
                 else low = mid + 1;
             }else{
-                if(nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                if(target >= nums[mid] && target <= nums[high]) low = mid + 1;
                 else high = mid - 1;
             }
         }
