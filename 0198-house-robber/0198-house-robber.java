4e9ccc9d43bf63236e42022ed9fec39a8010bdf2
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2026-01-23 19:49:54 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,21 @@
+/**
+ --> create an dp arr & init the 1st ele with nums 1st ele & 2nd dp ele will be max among nums 1st & 2nd
+ --> loop through nums & init dp[i] with max among prev ele of dp with sum of nums current ele and 
+     dps prev to prev's ele.
+ --> return dp length - 1
+ */
 class Solution {
     public int rob(int[] nums) {
-        int rob = 0;
-        int noRob = 0;
-        for(int i = 0;i < nums.length;i++){
-            int newRob = noRob + nums[i];
-            int noNewRob = Math.max(rob,noRob);
-            rob = newRob;
-            noRob = noNewRob;
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+
+        for(int i = 2;i < nums.length;i++){
+            dp[i] = Math.max(dp[i-1],nums[i] + dp[i - 2]);
         }
-        return Math.max(rob,noRob);
+        return dp[nums.length - 1];
     }
 }
