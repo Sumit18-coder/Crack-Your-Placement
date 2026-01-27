0da55db0494181d6441fde8464afa502dc3e80aa
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2026-01-27 18:32:31 +0530

    Time: 15 ms (82.1%), Space: 46.1 MB (94.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-     if(amount < 1) return 0;
-     //create a dp
-     int[] minCoinsDp = new int[amount + 1];
-     for(int i = 1;i <= amount; i++){
-        minCoinsDp[i] = Integer.MAX_VALUE;
-        for(int coin : coins)
-            if(coin <= i && minCoinsDp[i - coin] != Integer.MAX_VALUE)
-               minCoinsDp[i] = Math.min(minCoinsDp[i],1 + minCoinsDp[i - coin]); 
-     } 
-     if(minCoinsDp[amount] == Integer.MAX_VALUE)
-        return -1;
-     return minCoinsDp[amount];
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp,amount + 1);
+        dp[0] = 0;
+
+        for(int i = 1;i <= amount; i++){
+            for(int coin : coins){
+                if(i - coin >= 0){
+                    dp[i] = Math.min(dp[i],dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
