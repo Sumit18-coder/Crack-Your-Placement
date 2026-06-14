7fa5146d030cedea0888c50da990c5287dcbd115
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2026-06-14 10:02:48 +0530

    Time: 50 ms (5.93%), Space: 47.2 MB (39.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
         int[] res = new int[2];
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (nums[i] + nums[j] == target) {
+        for(int i = 0;i < nums.length;i++){
+            for(int j = i + 1;j < nums.length;j++){
+                if(nums[i] + nums[j] == target){
                     res[0] = i;
                     res[1] = j;
-                    return res; // Return as soon as the pair is found
                 }
             }
         }
-        return res; // In case no solution is found
+        return res;
     }
 }
