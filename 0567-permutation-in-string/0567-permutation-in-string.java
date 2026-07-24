6636303f5aff9495377f38183b59c89e44156448
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2026-07-24 20:37:00 +0530

    Time: 23 ms (29.38%), Space: 46.8 MB (18.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
 
-        int[] s1Count = new int[26];
-        int[] window = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            s1Count[c - 'a']++;
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for(char c : s1.toCharArray()){
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        int k = s1.length();
+        for(int i = 0;i < k;i++){
+            char ch = s2.charAt(i);
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
         }
 
-        for (int i = 0; i < s2.length(); i++) {
-            window[s2.charAt(i) - 'a']++;
+        if(map1.equals(map2)){
+            return true;
+        }
+        for(int i = k;i < s2.length();i++){
+            char add = s2.charAt(i);
+            map2.put(add, map2.getOrDefault(add, 0) + 1);
+            char remove = s2.charAt(i - k);
+            map2.put(remove, map2.get(remove) - 1);
 
-            if (i >= s1.length()) {
-                window[s2.charAt(i - s1.length()) - 'a']--;
+            if(map2.get(remove) == 0){
+                map2.remove(remove);
             }
-
-            if (matches(s1Count, window)) {
+            if(map1.equals(map2)){
                 return true;
             }
         }
         return false;
-    }
-
-    private boolean matches(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
     }
 }
