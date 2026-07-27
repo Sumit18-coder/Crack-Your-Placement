585ca160cdc4e34ed644f4ba3e377131b166c5ae
Sumit Avhale <114134204+Sumit18-coder@users.noreply.github.com>
2026-07-27 18:47:17 +0530

    Time: 1 ms (99.89%), Space: 45.4 MB (65.82%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,21 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-        int ans = 0;
-        for(int i = 0;i < chars.length;){
-            final char letter = chars[i];
+        int index = 0;//pos to write
+        int i = 0;//pos to read
+        while(i < chars.length){
+            char current = chars[i];
             int count = 0;
-        
-        while(i < chars.length && chars[i] == letter){
-            ++count;
-            ++i;
-        }
-        chars[ans++] = letter;
-        if(count > 1){
-            for(final char c : String.valueOf(count).toCharArray()){
-                chars[ans++] = c;
+            //count consective characters
+            while(i < chars.length && chars[i] == current){
+                count++;
+                i++;
+            }
+            //write the character
+            chars[index++] = current;
+
+            if(count > 1){
+                for(char ch : String.valueOf(count).toCharArray()){
+                    chars[index++] = ch;
+                }
             }
         }
-    }
-        return ans;
+        return index;
     }
 }
